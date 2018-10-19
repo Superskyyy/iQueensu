34f703b51dee95f3ea6966a36ac76b769ea79a12
Production <brainholers.com>
2018-10-19 17:30:34 -0400

    modified settings

--- iQueensu/settings.py
+++ iQueensu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l=zqe8o+tt8v6fyd*q-0+1_+_1440a$vmi--5vomh*j1jy4p8w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'iQueensu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'iQueensu',
+	'USER': 'iQueensu',
+	'PASSWORD':'Iqueensu.com123',
+	'HOST':'localhost',
+	'PORT':'',
     }
 }
 
@@ -118,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR,"static/")
+
