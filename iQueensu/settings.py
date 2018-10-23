6c6c5c1561c27bbe4ed268b1800f0af54eec39b5
MatrixDoge <xywhpanda@gmail.com>
2018-10-23 02:08:37 -0400

    Fixed the structure of the app and added a helloworld page that we can start working with.
    Signed-off-by: MatrixDoge <xywhpanda@gmail.com>

--- iQueensu/settings.py
+++ iQueensu/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'indexPage.apps.IndexpageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
