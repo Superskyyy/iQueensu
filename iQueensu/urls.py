2a3fa4f7f819894b2018564a031991b13ee24978
Superskyyy <15yc9@queensu.ca>
2018-10-23 18:24:07 -0400

    removed bad static

--- iQueensu/urls.py
+++ iQueensu/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('indexPage/',include('indexPage.urls')),
