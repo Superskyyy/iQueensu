09b064a56f38d93abe9531c43c754f86ba4b616b
MatrixDoge <xywhpanda@gmail.com>
2018-10-24 19:01:00 -0400

    Added ReactJS frontend example
    Signed-off-by: MatrixDoge <xywhpanda@gmail.com>

--- indexPage/views.py
+++ indexPage/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.conf import settings
 # Create your views here.
 
 def index(request):
-    return render(request,'indexPage/index.html')
+    if (request.GET.get('dev') == '2'):
+        return render(request,'indexPage/index.html', {'dev': { 'test': True, 'api': settings.REST_API_ADDRESS}})
+    else:
+        return HttpResponse("Coming soon", status=418)
