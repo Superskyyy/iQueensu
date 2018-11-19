f6f7c01766a15ceb2f7a000e838f973f5a3efd0f
NaArashi <calibur.tonyt@gmail.com>
2018-11-19 16:19:12 -0500

    Started a new Django app

---
+++ QCumber_Scraper/Spider.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from selenium.webdriver.support.ui import WebDriverWait
+
+from QCumber_Scraper.settings.Settings import *
+
+
+class spider:
+    __SCRAPER_DRIVER_DIR = None
+
+    def __init__(self):
+        self.SCRAPER_DRIVER_DIR = self.find_driver(SCRAPER_DRIVER)
+        assert self.SCRAPER_DRIVER_DIR is not None, 'Driver not found!'
+
+        option = webdriver.FirefoxOptions()
+
+        if not (SCRAPER_LOCAL_TEST and SCRAPER_DEBUG):
+            option.add_argument('-headless')
+        option.add_argument('--disable-gpu')
+
+        with webdriver.Firefox(options=option, executable_path=self.SCRAPER_DRIVER_DIR) as driver:
+            driver.get('https://my.queensu.ca')
+
+            wait = WebDriverWait(driver, 10)
+            wait.until(presence_of_element_located((By.ID, 'username')))
+
+            driver.find_element_by_id('username').send_keys(SCRAPER_USER_NAME)
+            driver.find_element_by_id('password').send_keys(SCRAPER_USER_PASSWD)
+
+            driver.find_element_by_name('_eventId_proceed').click()
+
+            if SCRAPER_DEBUG:
+                driver.get_screenshot_as_file('test.png')
+
+            print("Logged in!")
+
+            input()
+
+    def find_driver_raw(self, destiny):
+        print("Start searching driver under available path")
+        for path in os.sys.path:
+            for rel_path, dirs, files in os.walk(path):
+                if destiny in files:
+                    print("Driver", destiny, "found!")
+                    return os.path.join(path, rel_path, destiny)
+        return None
+
+    def find_driver(self, destiny):
+        config = Settings(driver_path=destiny)
+        try:
+            print("Start validating driver")
+            config.load_from_file()
+            if not os.path.exists(config.data['driver_path']):
+                config.data['driver_path'] = self.find_driver_raw(destiny)
+                config.save_to_file()
+        except:
+            config.data['driver_path'] = self.find_driver_raw(destiny)
+            config.save_to_file()
+
+        print("Driver", SCRAPER_DRIVER, "located at", config.data['driver_path'])
+        return config.data['driver_path']
--- iQueensu/urls.py
+++ iQueensu/urls.py
@@ -13,14 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include, url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('indexPage/',include('indexPage.urls')),
-    path('qapi_v0/',include('QAPI.urls')),
-    path('qauth_v0/',include('QAuth.urls')),
+    path('indexPage/', include('indexPage.urls')),
+    path('qapi_v0/', include('QAPI.urls')),
+    path('qauth_v0/', include('QAuth.urls')),
+    path('qcumber/', include('QCumber.urls'))
     ]
 
 #Add URL maps to redirect the base URL to our application
