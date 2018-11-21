48693ddf8d3dd525c5628ef31fb86184ee75f507
Superskyyy <15yc9@queensu.ca>
2018-11-21 16:47:36 -0500

    Override result-worker.
    Signed-off-by: superskyyy <15yc9@queensu.ca>

--- indexPage/views.py
+++ indexPage/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.conf import settings
+# noinspection PyPackageRequirements
 import json
+
+from django.conf import settings
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
 # Create your views here.
 
 def index(request):
