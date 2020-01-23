557eb1518aad1bf6cf8438be0a3f97178fa26ed6
Superskyyy <15yc9@queensu.ca>
2020-01-23 17:11:22 -0500

    format test

--- QCumber/scraper/spider.py
+++ QCumber/scraper/spider.py
@@ -3,6 +3,7 @@ This is a selenium spider that crawls all the course catalog data
 from our SOLUS system.
 """
 import logging
+import os
 import queue
 import random
 import sys
@@ -17,19 +18,35 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.support.ui import WebDriverWait
 
-from QCumber.scraper.assets.models import Course, \
-    CourseDetail, AcademicGroupPossibleValues, AcademicOrganizationPossibleValues, CampusPossibleValues, \
-    CareerPossibleValues, Components, CourseDescription, EnrollmentInformation, GradingPossibleValues, \
-    SubjectPossibleValues
+from QCumber.scraper.assets.models import (
+    Course,
+    CourseDetail,
+    AcademicGroupPossibleValues,
+    AcademicOrganizationPossibleValues,
+    CampusPossibleValues,
+    CareerPossibleValues,
+    Components,
+    CourseDescription,
+    EnrollmentInformation,
+    GradingPossibleValues,
+    SubjectPossibleValues,
+)
 
 try:
-    from QCumber.scraper.assets.settings import *
+    from QCumber.scraper.assets.settings import (
+        SCRAPER_USER_NAME,
+        SCRAPER_USER_PASSWD,
+        SCRAPER_DEBUG,
+        SCRAPER_LOCAL_TEST,
+    )
 except ModuleNotFoundError as error:
-    print('''
+    print(
+        """
         Please copy QCumber/scraper/assets/settings_example.py as settings.py in the same folder, 
         and edit the settings as instructed in the file. 
         #Notice, settings.py is ignored by .gitignore file, everything in that file WILL BE LOST in CVS.
-    ''')
+    """
+    )
     sys.exit(1)  # force to stop
 
     # make PyCharm feel happy even if there's no settings.py
@@ -43,6 +60,7 @@ class Spider:
     """
     Main spider class.
     """
+
     __SCRAPER_DRIVER_DIR = None
 
     def __init__(self):
@@ -65,11 +83,13 @@ class Spider:
         flush_db()
 
         # logging stuff
-        logging.basicConfig(level=logging.DEBUG,
-                            format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                            datefmt='%m-%d %H:%M',
-                            filename='spider.log',
-                            filemode='w')
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+            datefmt="%m-%d %H:%M",
+            filename="spider.log",
+            filemode="w",
+        )
 
         self.logger = logging.getLogger("QCumber_Scraper")
         self.logger.setLevel(logging.DEBUG)
@@ -78,7 +98,7 @@ class Spider:
         handler = logging.StreamHandler()
         listener = QueueListener(que, handler)
         self.logger.addHandler(queue_handler)
-        formatter = logging.Formatter('%(name)s - %(asctime)s: %(message)s')
+        formatter = logging.Formatter("%(name)s - %(asctime)s: %(message)s")
         handler.setFormatter(formatter)
         listener.start()
 
@@ -91,9 +111,9 @@ class Spider:
 
         self.option = webdriver.FirefoxOptions()
 
-        if not (SCRAPER_LOCAL_TEST and SCRAPER_DEBUG):
-            self.option.add_argument('-headless')
-        self.option.add_argument('--disable-gpu')
+        if not SCRAPER_LOCAL_TEST:
+            self.option.add_argument("-headless")
+        self.option.add_argument("--disable-gpu")
 
     @staticmethod
     def solus_random_wait(start: int, stop: int, step: float):
@@ -113,25 +133,35 @@ class Spider:
         """
         time.sleep(3)
 
-        with webdriver.Remote(options=self.option, command_executor="http://chrome:4444/wd/hub",
-                              desired_capabilities=DesiredCapabilities.CHROME) as driver:
+        with webdriver.Remote(
+            options=self.option,
+            command_executor="http://chrome:4444/wd/hub",
+            desired_capabilities=DesiredCapabilities.CHROME,
+        ) as driver:
             driver.get(
-                'https://saself.ps.queensu.ca/psc/saself/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSS_BROWSE_CATLG_P.GBL')
+                "https://saself.ps.queensu.ca/psc/saself/EMPLOYEE/HRMS/"
+                "c/SA_LEARNER_SERVICES.SSS_BROWSE_CATLG_P.GBL"
+            )
 
             wait = WebDriverWait(driver, 10)
-            wait.until(presence_of_element_located((By.ID, 'username')))
+            wait.until(presence_of_element_located((By.ID, "username")))
 
-            driver.find_element_by_id('username').send_keys(SCRAPER_USER_NAME)
-            driver.find_element_by_id('password').send_keys(SCRAPER_USER_PASSWD)
-            driver.find_element_by_name('_eventId_proceed').click()
+            driver.find_element_by_id("username").send_keys(SCRAPER_USER_NAME)
+            driver.find_element_by_id("password").send_keys(SCRAPER_USER_PASSWD)
+            driver.find_element_by_name("_eventId_proceed").click()
 
             if SCRAPER_DEBUG:
                 driver.get_screenshot_as_file(
-                    time.strftime('%Y.%m.%d', time.localtime(time.time())) + '_test.png')
+                    time.strftime("%Y.%m.%d", time.localtime(time.time())) + "_test.png"
+                )
 
             print("Login Successful")
             self.logger.info("Logged in!")
-            wait.until(presence_of_element_located((By.ID, 'DERIVED_SSS_BCC_SSS_EXPAND_ALL$97$')))
+            wait.until(
+                presence_of_element_located(
+                    (By.ID, "DERIVED_SSS_BCC_SSS_EXPAND_ALL$97$")
+                )
+            )
             driver.find_element_by_id("DERIVED_SSS_BCC_SSS_EXPAND_ALL$97$").click()
 
             # first get a list of subject full names
@@ -141,7 +171,8 @@ class Spider:
             while True:
                 try:
                     subject_full_name = driver.find_element_by_id(
-                        "DERIVED_SSS_BCC_GROUP_BOX_1$147$$" + str(counter)).text
+                        "DERIVED_SSS_BCC_GROUP_BOX_1$147$$" + str(counter)
+                    ).text
                     subject_full_names.append(subject_full_name)
                 except exceptions.NoSuchElementException:
                     break
@@ -154,10 +185,15 @@ class Spider:
                 detail_dict = {}
                 # IMPORTANT NOTE > We need to
                 # KEEP THE CALL TIME INTERVAL HIGH ENOUGH TO PREVENT DETECTION
-                wait_quick = WebDriverWait(driver, 35,
-                                           poll_frequency=self.solus_random_wait(1, 3, 0.5))
+                wait_quick = WebDriverWait(
+                    driver, 35, poll_frequency=self.solus_random_wait(1, 3, 0.5)
+                )
                 # try:
-                wait_quick.until(presence_of_element_located((By.ID, "CRSE_NBR$" + str(course_nbr_id))))
+                wait_quick.until(
+                    presence_of_element_located(
+                        (By.ID, "CRSE_NBR$" + str(course_nbr_id))
+                    )
+                )
 
                 item = driver.find_element_by_id("CRSE_NBR$" + str(course_nbr_id))
                 course_nbr = item.text
@@ -166,13 +202,19 @@ class Spider:
                     self.logger.info("Unspecified found, ignored for now. - Sky")
                     course_nbr_id += 1
                     continue
-                course_title = driver.find_element_by_id("CRSE_TITLE$" + str(course_nbr_id)).text
+                course_title = driver.find_element_by_id(
+                    "CRSE_TITLE$" + str(course_nbr_id)
+                ).text
                 if "***" in course_title:
-                    self.logger.info("Multiple offering course found, ignored for now. - Sky")
+                    self.logger.info(
+                        "Multiple offering course found, ignored for now. - Sky"
+                    )
                     course_nbr_id += 1
                     continue
 
-                self.logger.info("course#: " + course_nbr + " course title: " + course_title)
+                self.logger.info(
+                    "course#: " + course_nbr + " course title: " + course_title
+                )
 
                 # FIXME: this subject needs special attention
 
@@ -182,43 +224,60 @@ class Spider:
                 item.click()
 
                 # data prepare
-                wait_quick.until(presence_of_element_located(
-                    (By.ID, 'SSR_CRSE_OFF_VW_ACAD_CAREER$0')))
+                wait_quick.until(
+                    presence_of_element_located(
+                        (By.ID, "SSR_CRSE_OFF_VW_ACAD_CAREER$0")
+                    )
+                )
 
                 detail_dict["subject_code"] = driver.find_element_by_id(
-                    "DERIVED_CRSECAT_DESCR200").text[:4]
+                    "DERIVED_CRSECAT_DESCR200"
+                ).text[:4]
                 for names in subject_full_names:
                     if detail_dict["subject_code"] in names:
                         detail_dict["subject_name"] = names[7:]
 
                 detail_dict["career"] = driver.find_element_by_id(
-                    "SSR_CRSE_OFF_VW_ACAD_CAREER$0").text
+                    "SSR_CRSE_OFF_VW_ACAD_CAREER$0"
+                ).text
                 detail_dict["units"] = driver.find_element_by_id(
-                    "DERIVED_CRSECAT_UNITS_RANGE$0").text
+                    "DERIVED_CRSECAT_UNITS_RANGE$0"
+                ).text
                 detail_dict["grading"] = driver.find_element_by_id(
-                    "SSR_CRSE_OFF_VW_GRADING_BASIS$0").text
+                    "SSR_CRSE_OFF_VW_GRADING_BASIS$0"
+                ).text
                 detail_dict["components_description"] = driver.find_element_by_id(
-                    "DERIVED_CRSECAT_DESCR$0").text
+                    "DERIVED_CRSECAT_DESCR$0"
+                ).text
                 detail_dict["campus"] = driver.find_element_by_id(
-                    "CAMPUS_TBL_DESCR$0").text
+                    "CAMPUS_TBL_DESCR$0"
+                ).text
                 detail_dict["academic_group"] = driver.find_element_by_id(
-                    "ACAD_GROUP_TBL_DESCR$0").text
+                    "ACAD_GROUP_TBL_DESCR$0"
+                ).text
                 detail_dict["academic_org"] = driver.find_element_by_id(
-                    "ACAD_ORG_TBL_DESCR$0").text
+                    "ACAD_ORG_TBL_DESCR$0"
+                ).text
                 detail_dict["enroll_add_consent"] = driver.find_element_by_id(
-                    "SSR_CRSE_OFF_VW_CONSENT$0").text
+                    "SSR_CRSE_OFF_VW_CONSENT$0"
+                ).text
                 detail_dict["enroll_drop_consent"] = driver.find_element_by_id(
-                    "SSR_CRSE_OFF_VW_SSR_DROP_CONSENT$0").text
+                    "SSR_CRSE_OFF_VW_SSR_DROP_CONSENT$0"
+                ).text
                 # FIXME add enrollment prerequisites
                 detail_dict["course_description"] = driver.find_element_by_id(
-                    "SSR_CRSE_OFF_VW_DESCRLONG$0").text
+                    "SSR_CRSE_OFF_VW_DESCRLONG$0"
+                ).text
                 detail_dict["course_title"] = course_title
                 detail_dict["course_number"] = course_nbr
 
                 # this line seems useless
-                wait_quick.until(presence_of_element_located(
-                    (By.ID, 'DERIVED_SAA_CRS_RETURN_PB$163$')))
-                driver.find_element_by_id('DERIVED_SAA_CRS_RETURN_PB$163$').click()
+                wait_quick.until(
+                    presence_of_element_located(
+                        (By.ID, "DERIVED_SAA_CRS_RETURN_PB$163$")
+                    )
+                )
+                driver.find_element_by_id("DERIVED_SAA_CRS_RETURN_PB$163$").click()
 
                 # save data to django model
                 # print(detail_dict.values())
@@ -256,28 +315,39 @@ class Spider:
         course_object = Course(
             details=CourseDetail.objects.create(
                 career=CareerPossibleValues.objects.get_or_create(
-                    career=detail_dict["career"])[0],
+                    career=detail_dict["career"]
+                )[0],
                 units=detail_dict["units"],
                 grading_basis=GradingPossibleValues.objects.get_or_create(
-                    grading=detail_dict["grading"])[0],
+                    grading=detail_dict["grading"]
+                )[0],
                 course_components=Components.objects.get_or_create(
-                    description=detail_dict["components_description"])[0],
+                    description=detail_dict["components_description"]
+                )[0],
                 campus=CampusPossibleValues.objects.get_or_create(
-                    campus=detail_dict["campus"])[0],
+                    campus=detail_dict["campus"]
+                )[0],
                 academic_group=AcademicGroupPossibleValues.objects.get_or_create(
-                    academic_group=detail_dict["academic_group"])[0],
+                    academic_group=detail_dict["academic_group"]
+                )[0],
                 academic_organization=AcademicOrganizationPossibleValues.objects.get_or_create(
-                    academic_organization=detail_dict["academic_org"])[0],
+                    academic_organization=detail_dict["academic_org"]
+                )[
+                    0
+                ],
                 enrollment=EnrollmentInformation.objects.get_or_create(
                     enroll_add_consent=detail_dict["enroll_add_consent"],
-                    enroll_drop_consent=detail_dict["enroll_drop_consent"])[0],
+                    enroll_drop_consent=detail_dict["enroll_drop_consent"],
+                )[0],
                 description=CourseDescription.objects.get_or_create(
-                    description=detail_dict["course_description"])[0], ),
+                    description=detail_dict["course_description"]
+                )[0],
+            ),
             name=detail_dict["course_title"],
             number=detail_dict["course_number"],
             subject=SubjectPossibleValues.objects.get_or_create(
-                code=detail_dict['subject_code'],
-                name=detail_dict["subject_name"])[0]
+                code=detail_dict["subject_code"], name=detail_dict["subject_name"]
+            )[0],
         )
         course_object.save()
 
@@ -288,7 +358,9 @@ class Spider:
         :return:
         """
         print("Current OS version: " + sys.platform)
-        source = os.environ["PATH"].split(";" if sys.platform.__contains__("win") else ":")
+        source = os.environ["PATH"].split(
+            ";" if sys.platform.__contains__("win") else ":"
+        )
         os.sys.path.extend(source)
 
     @staticmethod
@@ -301,7 +373,7 @@ class Spider:
         new_spider.solus_spider()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     my_spider = Spider()
     # my_spider.scraper_start()
     my_spider.save_to_model({})
